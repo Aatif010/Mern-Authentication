649214bd84b40b48b9dfde95292a341d90566619
Aatif010 <aatifhussain8@gmail.com>
2024-03-22 02:16:04 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import {BrowserRouter, Routes, Route, Router} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  return  <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element = {<Home/>} />
+          <Route path='/about' element = {<About />} />
+          <Route path='/sign-in' element = {<Signin/>} />
+          <Route path='/sign-up' element = {<Signup/>} />
+          <Route path='/profile' element = {<Profile/>} />
+        </Routes>
+  </BrowserRouter>
 }
 
 export default App
