b9055f0b12ed16c3800999bc5d0130bde1b71334
Aatif010 <aatifhussain8@gmail.com>
2024-03-24 02:13:48 +0530

    connection error

--- backend-api/index.js
+++ backend-api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 app.listen(3000, () => {
-  console.log("server running on port 4000");
+  console.log("server is running");
 });
+mongoose
+  .connect("process.env.MONGO_URI")
+  .then(() => {
+    console.log("connected t0 database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
