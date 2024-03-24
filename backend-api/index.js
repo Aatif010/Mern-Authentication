cf80c3f7ba0f1c31856736ae7cf76251cf487dc6
Aatif010 <aatifhussain8@gmail.com>
2024-03-24 16:19:25 +0530

    create signup api

--- backend-api/index.js
+++ backend-api/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,7 +15,10 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
