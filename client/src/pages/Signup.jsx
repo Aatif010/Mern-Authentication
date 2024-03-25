ed801ef212355cec3487fcc1fba672bbeac8a9cf
Aatif010 <aatifhussain8@gmail.com>
2024-03-25 16:06:25 +0530

    showing error something went wrong in signin page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
 
 function Signup() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value});
 
@@ -14,6 +15,7 @@ function Signup() {
     e.preventDefault();
     try {
       setLoading(true);
+      setError(false)
       const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: {
@@ -28,7 +30,7 @@ function Signup() {
         return;
       }
 
-      setError(false);
+      navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(true);
