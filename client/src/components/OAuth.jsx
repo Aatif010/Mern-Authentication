c100f362d1df1fd0b1686e7c6821984400431ac5
Aatif010 <aatifhussain8@gmail.com>
2024-03-26 02:48:23 +0530

    error i OAuth and store file

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function OAuth() {
+    const handleGoogleClick = async () => {
+        try {
+            
+        } catch (error) {
+            console.log("could not login with google",error);
+            
+        }
+    
+    
+  return (
+    <button type='button' onClick={handleGoogleClick} className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'>Continue with google</button>
+  )
+  
